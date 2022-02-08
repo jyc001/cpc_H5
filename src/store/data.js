40255efca1df29a9eb51298e7f8a002befc288ce
蒋奕成 <80881099@qq.com>
2022-02-08 17:45:25 +0800

    update data and image add copyright

--- src/store/data.js
+++ src/store/data.js
@@ -7,68 +7,436 @@ const cpc_data=[
         details:[
             {
                 title:"会议主题",
-                content:"a"
+                content:""
             },
             {
                 title: "重要决议",
-                content: "a"
+                content: ""
             },
             {
                 title: "重大变革",
-                content: "a",
+                content: "",
             },
             {
                 title: "重要人物",
-                content: "a"
+                content: ""
             }
         ],
     },
     {
         name: "中国共产党第二次全国代表大会",
         date: "1921年7月23日",
-        img: "01.jpg",
+        img: "02.jpg",
         details:[
             {
                 title:"会议主题",
-                content:"a"
+                content:""
             },
             {
                 title: "重要决议",
-                content: "a"
+                content: ""
             },
             {
                 title: "重大变革",
-                content: "a",
+                content: "",
             },
             {
                 title: "重要人物",
-                content: "a"
+                content: ""
             }
         ],
     },
     {
         name: "中国共产党第三次全国代表大会",
         date: "1921年7月23日",
-        img: "01.jpg",
+        img: "03.jpg",
         details:[
             {
                 title:"会议主题",
-                content:"a"
+                content:""
             },
             {
                 title: "重要决议",
-                content: "a"
+                content: ""
             },
             {
                 title: "重大变革",
-                content: "a",
+                content: "",
             },
             {
                 title: "重要人物",
-                content: "a"
+                content: ""
             }
         ],
-    }
+    },
+    {
+        name: "中国共产党第四次全国代表大会",
+        date: "1921年7月23日",
+        img: "04.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第五次全国代表大会",
+        date: "1921年7月23日",
+        img: "05.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第六次全国代表大会",
+        date: "1921年7月23日",
+        img: "06.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第七次全国代表大会",
+        date: "1921年7月23日",
+        img: "07.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第八次全国代表大会",
+        date: "1921年7月23日",
+        img: "08.gif",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第九次全国代表大会",
+        date: "1921年7月23日",
+        img: "09.gif",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十次全国代表大会",
+        date: "1921年7月23日",
+        img: "10.gif",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十一次全国代表大会",
+        date: "1921年7月23日",
+        img: "11.gif",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十二次全国代表大会",
+        date: "1921年7月23日",
+        img: "12.gif",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十三次全国代表大会",
+        date: "1921年7月23日",
+        img: "13.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十四次全国代表大会",
+        date: "1921年7月23日",
+        img: "14.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十五次全国代表大会",
+        date: "1921年7月23日",
+        img: "15.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十六次全国代表大会",
+        date: "1921年7月23日",
+        img: "16.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十七次全国代表大会",
+        date: "1921年7月23日",
+        img: "17.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十八次全国代表大会",
+        date: "1921年7月23日",
+        img: "18.jpg",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
+    {
+        name: "中国共产党第十九次全国代表大会",
+        date: "1921年7月23日",
+        img: "19.png",
+        details:[
+            {
+                title:"会议主题",
+                content:""
+            },
+            {
+                title: "重要决议",
+                content: ""
+            },
+            {
+                title: "重大变革",
+                content: "",
+            },
+            {
+                title: "重要人物",
+                content: ""
+            }
+        ],
+    },
 
 ]
 export default cpc_data;
